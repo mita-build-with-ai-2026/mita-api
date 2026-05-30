c5cc1f00048fcff53b89fbeffc1f9cfde1acd089
Denis <denislirachoque64@.com>
2026-05-30 12:47:11 -0400

    la pelota

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const router = Router();
 
 // Ruta de health check / verificación de estado
 router.get('/status', getStatus);
+router.get("/ping", (req, res) => res.send("pong"));
 router.get("/", (req, res) => res.send("hola ar1"));
 
 export default router;
